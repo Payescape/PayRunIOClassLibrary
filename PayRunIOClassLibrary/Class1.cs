86ae72f5551a6cc7d0e67b25f5a34146a333185c
Jim Borland <jamesb@payescape.com>
2020-06-18 10:27:02 +0100

    Re-Adding references.

--- PayRunIOClassLibrary/Class1.cs
+++ PayRunIOClassLibrary/Class1.cs
@@ -406,14 +406,14 @@ namespace PayRunIOClassLibrary
             string consumerKey = "9MaHMtmTUC6iMgymPl94g";                             //Original developer key : "m5lsJMpBnkaJw086zwDw"     "1UH6t3ikiWbdxTNT2Dg"
             string consumerSecret = "44sem3aVCUCxjaFmnolPQhPii7rQQwEyqgTnSJB655Q";   //Original developer secret : "GHM6x3xLEWujpLC5sGXKQ3r2j14RGI0eoLbab8w415Q"     "jKUX3lrQUe4KhEiox6IZw8CXnWUdAkyTl1kthR8ayQ"
             string url = "https://api.test.payrun.io";
-            RestApiHelper apiHelper = new PayRunIO.CSharp.SDK.RestApiHelper(
+            RestApiHelper apiHelper = new RestApiHelper(
                     new PayRunIO.OAuth1.OAuthSignatureGenerator(),
                     consumerKey,
                     consumerSecret,
                     url,
                     "application/xml",
                     "application/xml");
-            return apiHelper;
+           return apiHelper;
         }
         public void ArchiveDirectory(XDocument xdoc, string directory, string originalDirName, string archiveDirName)
         {
